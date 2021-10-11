1688bd860f8c664956ae8b84acb30ab16af18530
GuadalupeArias <guadalupe.arias90@gmail.com>
2021-10-11 14:44:26 -0300

    Se instala Flask-CORS

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.auto import Auto, AutoList, AutoId
 
 
-
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL2', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
